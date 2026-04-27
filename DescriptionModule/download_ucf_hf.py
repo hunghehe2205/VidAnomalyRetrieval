d4a06fa8d0921001ba3ec3e69e28f6af9c8bc504
hungHCMUT <hung.nguyen2205@hcmut.edu.vn>
2026-04-27 16:16:01 +0700

    ppush

--- DescriptionModule/download_ucf_hf.py
+++ DescriptionModule/download_ucf_hf.py
@@ -1,11 +1,17 @@
-#!/usr/bin/env python3
-import os
-from pathlib import Path
-from dotenv import load_dotenv
 from huggingface_hub import snapshot_download
 
-load_dotenv(Path(__file__).resolve().parents[1] / ".env")
-token = os.environ["HF_READ_TOKEN"]
+hf_token = ""
+# dataset
+snapshot_download(
+    repo_id="hungnghehe/UCFClipFeature",
+    repo_type="dataset",
+    local_dir="UCFClipFeature",
+    token=hf_token,
+)
 
-snapshot_download(repo_id="hungnghehe/UCFClipFeature", local_dir="UCFClipFeature", token=token)
-snapshot_download(repo_id="hungnghehe/Org_vadclip", local_dir="Org_vadclip", token=token)
+# model
+snapshot_download(
+    repo_id="hungnghehe/Org_vadclip",
+    local_dir="Org_vadclip",   # mặc định là model
+    token=hf_token,
+)
